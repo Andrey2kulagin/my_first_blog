57bfe51b939dba9f037aded1008de61f05ee9285
Электронный Зал <elzal@ad.guu.ru>
2023-02-13 19:00:35 +0300

    a

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from .views import  PostsListView, PostDetailView
 urlpatterns = [
     path('', PostsListView.as_view()),
-    path('<pk>/', PostDetailView.as_view()),
+    path('<pk>/s', PostDetailView.as_view()),
 ]
